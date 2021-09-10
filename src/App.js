4ec9c31ccfbe6e5baa6fb01a6dc50ea48fb335d3
Franco Nahuel Ibañez <franco.nahuel.ibanez@gmail.com>
2021-09-10 00:21:25 -0300

    consuta a la api agregada

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Formulario } from './components/Formulario';
 import { Header } from './components/Header'
 
 
 export const App = () => {
+    
+    const [categoria, guardarCategoria] = useState('general');
+    const [noticias, guardarNoticias] = useState([])
+
+
+    
+    useEffect(() => {
+        const consultarApi = async () => {
+            const url = `https://newsapi.org/v2/top-headlines?country=ar&category=${categoria}&apiKey=cc21880c648545229cddd19ebf00fc65`
+
+            const req = await fetch(url)
+            const noticias = await req.json();
+
+            guardarNoticias(noticias)
+        }
+
+        consultarApi()
+
+    }, [categoria])
+    
     return (
         <>
             <Header titulo="Buscador de noticias" />        
         
             <div className="container white">
-                <Formulario />
+                <Formulario 
+                    guardarCategoria={ guardarCategoria }
+                />
             </div>
         
         
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,12 +1,38 @@
 import React from 'react';
+import { useSelect } from '../hooks/useSelect';
 import styles from './Formulario.module.css'
 
-export const Formulario = () => {
+
+
+export const Formulario = ({guardarCategoria}) => {
+
+    const OPCIONES = [
+        {value: 'general', label: 'General'},
+        {value: 'business', label: 'Negocios'},
+        {value: 'entertainment', label: 'Entretenimiento'},
+        {value: 'health', label: 'Salud'},
+        {value: 'science', label: 'Ciencia'},
+        {value: 'sports', label: 'Deportes'},
+        {value: 'technology', label: 'Tecnologia'},
+    ]
+
+
+    const [categoria, SelectNoticias] = useSelect('general', OPCIONES);
+
+    //buscar noticias
+    const buscarNoticas = e =>{
+        e.preventeDefault()
+
+        guardarCategoria( categoria )
+    }
+
+
     return (
         <div className={` ${styles.buscador} row`}>
             <div className="col s12 m8 offset-m2">
-                <form>
+                <form onSubmit={ buscarNoticas }>
                     <h2 className={styles.heading}>Encuentra noticias por categoria</h2>
+                    <SelectNoticias />
 
                     <div className="input-field col s12">
                         
